c162f953385b8b1f4849404fbb7e7fb33be07fa8
Andreu Garcia <andreustreaming422@gmail.com>
2022-05-05 19:43:33 +0200

    fish

---
+++ fish/config.fish
@@ -0,0 +1,13 @@
+set fish_greeting ""
+
+# Aliases
+
+alias grep "grep --color=auto"
+alias ccat "ccat -G Plaintext="blink" -G Keyword="purple" -G String="darkgreen" -G Punctuation="brown" -G Comment="faint""
+alias ls "exa --group-directories-first"
+alias tree "exa -T"
+alias 100%hacker "neofetch" &
+
+alias cat "bat --theme ansi -f --paging=never"
+
+
